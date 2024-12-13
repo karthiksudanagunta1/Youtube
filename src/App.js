6a76a668afb6e8234218ecbdd77fecf4350e4665
karthik <chowdarykarthik858@gmail.com>
2024-12-13 08:52:43 +0530

    p added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="bg-black text-white h-full w-full flex-row justify-center">
      <h1 className='m-auto text-gray-50'>Youtube</h1>
+     <p>add the button</p>
     </div>
   );
 }
