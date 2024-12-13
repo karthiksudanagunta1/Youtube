b8f58765d64f29ec4ccd3477778fe1afa6c34dad
karthik <chowdarykarthik858@gmail.com>
2024-12-13 07:39:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black text-white h-full w-full flex-row justify-center">
+     <h1 className='m-auto text-gray-50'>Youtube</h1>
+    </div>
+  );
+}
+
+export default App;
